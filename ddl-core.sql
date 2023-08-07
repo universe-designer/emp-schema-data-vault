b7737ae2add6378de0add84b3fad750618d7bd23
Daniel Fritzler <118835568+universe-designer@users.noreply.github.com>
2023-08-07 18:13:32 +0200

    Update ddl-core.sql

--- ddl-core.sql
+++ ddl-core.sql
@@ -82,3 +82,34 @@
 	"EMPNO_HASHKEY" RAW(16), 
 	"DEPTNO_HASHKEY" RAW(16)
    ) ;
+--------------------------------------------------------
+--  DDL for Table SAT_LINK_WORKS_EFF
+--------------------------------------------------------
+
+CREATE TABLE SAT_LINK_WORKS_EFF(
+ "WORKS_HASHKEY" RAW(16), 
+ "BEGIN_DATE" DATE,
+ "END_DATE" DATE,
+ "LOAD_DATE" DATE, 
+ "RECORDSOURCE" VARCHAR2(20)
+);
+
+--------------------------------------------------------
+--  DDL for View V_SAT_LINK_WORKS_EFF
+--------------------------------------------------------
+--complete history / current flag
+CREATE OR REPLACE VIEW V_SAT_LINK_WORKS_EFF
+AS
+SELECT works_hashkey, TO_CHAR(begin_date, 'dd.mm.yyyy hh24:mi:ss') as begin_date, TO_CHAR(end_date, 'dd.mm.yyyy hh24:mi:ss') as end_date, load_date, current_record
+FROM (  --current links
+        SELECT e.*, 1 AS current_record
+        FROM SAT_LINK_WORKS_EFF e INNER JOIN   (--most recent link records
+                                                SELECT works_hashkey, MAX(load_date) AS load_date
+                                                FROM sat_link_works_eff
+                                                GROUP BY works_hashkey) c ON e.works_hashkey=c.works_hashkey AND e.load_date=c.load_date
+        WHERE e.end_date > sysdate
+        UNION ALL
+        --historic links
+        SELECT h.*, 0 AS current_record
+        FROM SAT_LINK_WORKS_EFF h
+        WHERE h.end_date < sysdate);
